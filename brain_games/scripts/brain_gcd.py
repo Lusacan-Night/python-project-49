5e77ae2c24aad3da843218f5b16a4da51f645ad5
Sultan <lusacan.night@gmail.com>
2023-12-02 20:32:44 +0400

    add new game brain_gcd

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,12 @@
+from .cli import welcome_user
+from brain_games.games.games import game_loop
+
+
+def main():
+    name = welcome_user()
+    print('Find the greatest common divisor of given numbers.')
+    game_loop(name, 'gcd')
+
+
+if __name__ == '__main__':
+    main()
