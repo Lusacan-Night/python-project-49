e5850dbdcf49d0b289e71ffa8cac5267b2c976b5
Sultan <lusacan.night@gmail.com>
2023-12-03 12:17:48 +0400

    refactor

--- brain_games/games/games.py
+++ brain_games/games/games.py
@@ -2,100 +2,19 @@ import operator
 import prompt
 
 from brain_games.scripts.cli import welcome_user
-from math import gcd
-from random import randint, choice
+from brain_games.games.utils import question, wrong_answer
 
-
-def gen_random_number(start, end):
-    return randint(start, end)
-
-
-def is_even(number):
-    return 'yes' if number % 2 == 0 else 'no'
-
-
-def pick_random_operator():
-    return choice(['*', '+', '-'])
-
-
-def question(question):
-    return print(f'Question: {question}')
-
-
-def even_game():
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    start = 1
-    end = 100
-    question_number = gen_random_number(start, end)
-    question(question_number)
-    return is_even(question_number)
-
-
-def calculate_game():
-    print('What is the result of the expression?')
-    start = 0
-    end = 100
-    first = gen_random_number(start, end)
-    second = gen_random_number(start, end)
-    op = pick_random_operator()
-    print(f'Question: {first} {op} {second}')
-
-    match op:
-        case '*':
-            return str(operator.mul(first, second))
-        case '+':
-            return str(operator.add(first, second))
-        case '-':
-            return str(operator.sub(first, second))
-
-
-def gcd_game():
-    print('Find the greatest common divisor of given numbers.')
-    start = 0
-    end = 100
-    first = gen_random_number(start, end)
-    second = gen_random_number(start, end)
-    print(f'Question: {first} {second}')
-    return str(gcd(first, second))
-
-
-def progression_game():
-    print('What number is missing in the progression?')
-
-    start = gen_random_number(1, 5)
-    end = gen_random_number(25, 50)
-    step = gen_random_number(1, 5)
-
-    sequence = [str(x) for x in range(start, end, step)]
-    random_num = choice(sequence)
-    index_position = sequence.index(random_num)
-    sequence[index_position] = '..'
-    print(f'Question: {" ".join(sequence)}')
-    return random_num
-
-
-def prime_game():
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    start = 1
-    end = 200
-    step = -1
-    random_num = gen_random_number(start, end)
-    print(f'Question: {random_num}')
-
-    for i in range(random_num - 1, 1, step):
-        if random_num % i == 0:
-            return 'no'
-    return 'yes'
-
-
-def wrong_answer(correct, answer):
-    print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct}'.")
+from .gcd import gcd_game
+from .even import even_game
+from .calc import calculate_game
+from .prime import prime_game
+from .progression import progression_game
 
 
 def game_loop(game):
     name = welcome_user()
-    tries_count = 3
-    for i in range(tries_count):
+    TRIES_COUNT = 3
+    for i in range(TRIES_COUNT):
         match game:
             case 'even':
                 correct_answer = even_game()
