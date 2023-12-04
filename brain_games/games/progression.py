d0d25db4423168903259f400433f29c989db78e8
Sultan <lusacan.night@gmail.com>
2023-12-04 21:36:42 +0400

    refactor constants

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,7 +3,8 @@ from brain_games.games.utils import get_random_number
 
 
 def progression_game():
-    start, end, step = get_random_number(1, 5), get_random_number(25, 50), get_random_number(1, 5)
+    start, end = get_random_number(1, 5), get_random_number(25, 50)
+    step = get_random_number(1, 5)
     sequence = [str(x) for x in range(start, end, step)]
     random_num = choice(sequence)
     index_position = sequence.index(random_num)
