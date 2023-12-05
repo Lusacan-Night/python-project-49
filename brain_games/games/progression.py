6763f67d05db59e8911a3fb9b2c4948ba40a925f
Sultan <lusacan.night@gmail.com>
2023-12-05 20:22:43 +0400

    fix error

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -10,18 +10,7 @@ def progression_game():
 
 def crypt_sequence(sequence):
     crypted_sequence = sequence[:]
-    random_num = choice(crypted_sequence)
-    index_position = crypted_sequence.index(random_num)
+    crypted_number = choice(crypted_sequence)
+    index_position = crypted_sequence.index(crypted_number)
     crypted_sequence[index_position] = '..'
-    return crypted_sequence
-
-
-def decrypt_sequence(sequence):
-    sequence_start = sequence[0]
-    sequence_end = len(sequence) - 1
-    result = round((int(sequence[1]) - int(sequence[0])) / 2)
-    if (sequence_start == '..'):
-        result = sequence[1] - result
-    if (sequence_end == '..'):
-        result = sequence[:-2] + result
-    return str(result)
+    return (crypted_sequence, crypted_number)
