ba5419a068688461a39259d038fcc0362b7e6573
Sultan <lusacan.night@gmail.com>
2023-12-05 20:40:51 +0400

    fix prime game

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -9,6 +9,10 @@ def prime_game():
 def calculate_prime(number):
     step = -1
     range_end = 1
+
+    if number == 1:
+            return 'no'
+
     for i in range(number - 1, range_end, step):
         if number % i == 0:
             return 'no'
