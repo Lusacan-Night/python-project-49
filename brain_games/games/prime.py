cda1ea8b7a00c530762c826fcab56a6c29d427d0
Sultan <lusacan.night@gmail.com>
2023-12-09 15:03:18 +0400

    fix error

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -2,7 +2,7 @@ from random import randint
 
 
 def get_description():
-    return 'Answer "yes" if given number is prime. Otherwise answer "no"'
+    return 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def get_question():
